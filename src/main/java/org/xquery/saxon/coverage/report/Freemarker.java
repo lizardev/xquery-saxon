48519873a6377955d922b13cea500006672ce326
Krzysztof Tobola <krzysztof.tobola@ubs.com>
2014-07-10 14:10:14 +0200

    Use freemarker in ModuleReportHtmlPrinter

---
+++ src/main/java/org/xquery/saxon/coverage/report/Freemarker.java
@@ -0,0 +1,30 @@
+package org.xquery.saxon.coverage.report;
+
+import freemarker.cache.ClassTemplateLoader;
+import freemarker.template.Configuration;
+
+import java.io.StringWriter;
+import java.util.Map;
+
+public final class Freemarker {
+	private static final String TEMPLATE_PATH_PREFIX = "/";
+	private static final Configuration CONFIGURATION = new Configuration();
+
+	static {
+		CONFIGURATION.setTemplateLoader(new ClassTemplateLoader(Freemarker.class, TEMPLATE_PATH_PREFIX));
+		CONFIGURATION.setTemplateUpdateDelay(Integer.MAX_VALUE);
+	}
+
+	private Freemarker(){
+	}
+
+	public static String renderTemplate(String templatePath, Map<String, ?> rootMap) {
+		try {
+			StringWriter stringWriter = new StringWriter();
+			CONFIGURATION.getTemplate(templatePath).process(rootMap, stringWriter);
+			return stringWriter.toString();
+		} catch (Exception exception) {
+			throw new RuntimeException(exception);
+		}
+	}
+}
--- src/main/java/org/xquery/saxon/coverage/report/ModuleReportHtmlPrinter.java
+++ src/main/java/org/xquery/saxon/coverage/report/ModuleReportHtmlPrinter.java
@@ -1,73 +1,46 @@
 package org.xquery.saxon.coverage.report;
 
+import com.google.common.collect.ImmutableMap;
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang3.StringEscapeUtils;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import static org.xquery.saxon.coverage.report.Freemarker.renderTemplate;
 
 public class ModuleReportHtmlPrinter {
 
-    public String print(ModuleReport moduleReport) {
-        List<String> lines = readLines(moduleReport);
-        StringBuilder report = new StringBuilder();
-        for (int i = 1; i <= lines.size(); i++) {
-            LineReport lineReport = moduleReport.getLineReport(i);
-            //report.append(String.format("<tr class='%s'><td>%d</td><td><pre>%s</pre></td></tr>%n",
-            report.append(String.format("<tr class='%s'><td>%d</td><td><pre>%s</pre></td></tr>%n",
-                    getCssClass(lineReport), i, StringEscapeUtils.escapeHtml4(lines.get(i - 1))));
-        }
-        report.insert(0, "<html>\n" +
-                "<head>\n" +
-                "    <style type=\"text/css\">\n" +
-                "        .covered {\n" +
-                "            background: lightgreen;\n" +
-                "            font-family: monospace;\n" +
-                "        }\n" +
-                "\n" +
-                "        .not-covered {\n" +
-                "            background: lightpink;\n" +
-                "            font-family: monospace;\n" +
-                "        }\n" +
-                "\n" +
-                "        .not-instrumented {\n" +
-                "            font-family: monospace;\n" +
-                "        }\n" +
-                "        pre {\n" +
-                "            margin: 0;\n" +
-                "        }" +
-                "    </style>\n" +
-                "</head>\n" +
-                "<body>\n" +
-                "<table>");
-        report.append("</table>\n" +
-                "</body>\n" +
-                "</html>");
-        return report.toString();
-    }
+	public String print(ModuleReport moduleReport) {
+		List<String> lines = readLines(moduleReport);
+		List<LineReport> lineReports = new ArrayList<LineReport>();
 
-    private String getCssClass(LineReport lineReport) {
-        if (lineReport == null) return "not-instrumented";
-        return lineReport.isFullyCovered() ? "covered" : "not-covered";
-    }
+		for (int i = 1; i <= lines.size(); i++) {
+			LineReport lineReport = moduleReport.getLineReport(i);
+			lineReports.add(lineReport);
+		}
 
-    private List<String> readLines(ModuleReport moduleReport) {
-        try {
-            return FileUtils.readLines(getFile(moduleReport.getModule()));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+		Map<String, ?> params = ImmutableMap.of("lineReports", lineReports, "lines", lines);
+		return renderTemplate("/org/xquery/saxon/coverage/report/ModuleReport.ftl", params);
+	}
 
-    private File getFile(String module) {
-        try {
-            return new File(new URL(module).getFile());
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	private List<String> readLines(ModuleReport moduleReport) {
+		try {
+			return FileUtils.readLines(getFile(moduleReport.getModule()));
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
+	private File getFile(String module) {
+		try {
+			return new File(new URL(module).getFile());
+		} catch (MalformedURLException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
