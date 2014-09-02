8b89e6a837b9affa2ebb4177f1d6f661d2f9776e
sharky <grzegorz.dyk@gmail.com>
2014-09-02 22:00:23 +0200

    Tests made more rigorous

--- coverage/src/test/java/org/xquery/saxon/coverage/CoverageTest.java
+++ coverage/src/test/java/org/xquery/saxon/coverage/CoverageTest.java
@@ -1,42 +1,57 @@
 package org.xquery.saxon.coverage;
 
+import com.google.common.collect.Iterables;
+import org.hamcrest.CustomTypeSafeMatcher;
+import org.hamcrest.Matcher;
+import org.junit.Before;
 import org.junit.Test;
+import org.xquery.saxon.coverage.report.LineReport;
 import org.xquery.saxon.coverage.report.ModuleReport;
-import org.xquery.saxon.coverage.report.ModuleReportHtmlPrinter;
-import org.xquery.saxon.coverage.report.Report;
 import org.xquery.saxon.coverage.util.XQueryExecutor;
 
-import java.io.File;
-
-import static org.xquery.saxon.coverage.util.Utils.fileNameFromUri;
-import static org.xquery.saxon.coverage.util.Utils.writeStringToFile;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
 
 public class CoverageTest {
 
+    private CoverageService coverageService;
+    private XQueryExecutor xQueryExecutor;
+
+    @Before
+    public void initializeExecutorWithCoverage() {
+        coverageService = new CoverageService();
+        xQueryExecutor = new XQueryExecutor(coverageService.getTraceProvider());
+    }
+
     @Test
     public void testCoverageOfTourModule() {
-        CoverageService coverageService = new CoverageService();
-        XQueryExecutor xQueryExecutor = new XQueryExecutor(coverageService.getTraceProvider());
-
         xQueryExecutor.execute("/tour.xq");
 
-        printCoverageReport(coverageService.getReport());
+        assertThat(coverageService.getReport().getModuleReports(), hasSize(1));
+        ModuleReport moduleReport = Iterables.getFirst(coverageService.getReport().getModuleReports(), null);
+        assertThat(moduleReport.getLineCoverage(), is(closeTo(0.91, 0.01)));
+        assertThat(moduleReport.getNotFullyCoveredLines(), containsInAnyOrder(
+                line(310), line(312), line(51), line(55), line(68), line(73), line(322), line(152), line(195)));
     }
 
     @Test
     public void testCoverageOfTestModule() {
-        CoverageService coverageService = new CoverageService();
-        XQueryExecutor xQueryExecutor = new XQueryExecutor(coverageService.getTraceProvider());
-
         xQueryExecutor.execute("/test.xq");
 
-        printCoverageReport(coverageService.getReport());
+        assertThat(coverageService.getReport().getModuleReports(), hasSize(1));
+        ModuleReport moduleReport = Iterables.getFirst(coverageService.getReport().getModuleReports(), null);
+        assertThat(moduleReport.getNotFullyCoveredLines(), contains(line(6)));
+        assertThat(moduleReport.getLineCoverage(), is(closeTo(0.80, 0.01)));
+
     }
 
-    private static void printCoverageReport(Report report) {
-        for (ModuleReport moduleReport : report.getModuleReports()) {
-            System.out.printf("Coverage of %s is: %.2f%n", moduleReport.getModule(), moduleReport.getLineCoverage());
-            writeStringToFile(new File(fileNameFromUri(moduleReport.getModule()) + ".html"), new ModuleReportHtmlPrinter().print(moduleReport));
-        }
+    private static Matcher<LineReport> line(final int lineNumber) {
+        return new CustomTypeSafeMatcher<LineReport>("line report with line number = " + lineNumber) {
+
+            @Override
+            protected boolean matchesSafely(LineReport lineReport) {
+                return lineReport.getLineNumber() == lineNumber;
+            }
+        };
     }
 }
