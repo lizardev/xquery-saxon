054e2909dbe9d177cae760e6bf63ded1a32c23aa
Adam Laczynski <adam.laczynski@gmail.com>
2014-11-15 20:46:46 +0100

    fix issue with file name

--- coverage/src/test/java/org/xquery/saxon/coverage/TestConstants.java
+++ coverage/src/test/java/org/xquery/saxon/coverage/TestConstants.java
@@ -2,9 +2,9 @@ package org.xquery.saxon.coverage;
 
 public final class TestConstants {
 
-    public static final ModuleUri TOUR_MODULE = ModuleUri.fromResourceName("/Tour.xq");
+    public static final ModuleUri TOUR_MODULE = ModuleUri.fromResourceName("/tour.xq");
     public static final ModuleUri ONE_LINE_MODULE = ModuleUri.fromResourceName("/OneLine.xq");
-    public static final ModuleUri FUNCTIONS_MODULE = ModuleUri.fromResourceName("/Functions.xq");
+    public static final ModuleUri FUNCTIONS_MODULE = ModuleUri.fromResourceName("/functions.xq");
     public static final ModuleUri TWO_BRANCHES_MODULE = ModuleUri.fromResourceName("/TwoBranches.xq");
     public static final ModuleUri IMPORT_MAIN_MODULE = ModuleUri.fromResourceName("/import/Main.xq");
     public static final ModuleUri IMPORT_LIBRARY_MODULE = ModuleUri.fromResourceName("/import/Library.xq");
