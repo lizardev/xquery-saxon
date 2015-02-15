4333c45277fdb4ace7530c65191ddf015ef1a19b
Michal Jedynak <m.jedynak@gmail.com>
2015-02-15 11:31:35 +0100

    one method to copy resources

--- coverage/src/main/java/com/github/lizardev/xquery/saxon/coverage/report/StaticResourceTransferor.java
+++ coverage/src/main/java/com/github/lizardev/xquery/saxon/coverage/report/StaticResourceTransferor.java
@@ -3,39 +3,29 @@ package com.github.lizardev.xquery.saxon.coverage.report;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URISyntaxException;
 
-import static org.apache.commons.io.FileUtils.copyFileToDirectory;
 import static org.apache.commons.io.FileUtils.copyInputStreamToFile;
 
 public class StaticResourceTransferor {
 
     private static final String BASE_PATH = "com/github/lizardev/xquery/saxon/coverage/report";
+    private static final String WEB_JARS_PATH = "META-INF/resources/webjars";
 
     public void copyResources(File baseDir) {
-        copyResourceFromWebjar("META-INF/resources/webjars/jquery/2.1.3/jquery.min.js", new File(baseDir + "/js/jquery.min.js"));
-        copyResourceFromWebjar("META-INF/resources/webjars/tablesorter/2.17.8/js/jquery.tablesorter.min.js", new File(baseDir + "/js/jquery.tablesorter.min.js"));
-        copyResourceFromWebjar("META-INF/resources/webjars/tablesorter/2.17.8/css/theme.blue.css", new File(baseDir + "/css/theme.blue.css"));
-        copyResourceFromWebjar("META-INF/resources/webjars/jQuery.FilterTable/1.5/jquery.filtertable.min.js", new File(baseDir + "/js/jquery.filtertable.min.js"));
-        copyResource(BASE_PATH + "/css/module-report.css", new File(baseDir + "/css"));
-        copyResource(BASE_PATH + "/css/module-report-index.css", new File(baseDir + "/css"));
+        copyResource(WEB_JARS_PATH + "/jquery/2.1.3/jquery.min.js", new File(baseDir + "/js/jquery.min.js"));
+        copyResource(WEB_JARS_PATH + "/tablesorter/2.17.8/js/jquery.tablesorter.min.js", new File(baseDir + "/js/jquery.tablesorter.min.js"));
+        copyResource(WEB_JARS_PATH + "/tablesorter/2.17.8/css/theme.blue.css", new File(baseDir + "/css/theme.blue.css"));
+        copyResource(WEB_JARS_PATH + "/jQuery.FilterTable/1.5/jquery.filtertable.min.js", new File(baseDir + "/js/jquery.filtertable.min.js"));
+        copyResource(BASE_PATH + "/css/module-report.css", new File(baseDir + "/css/module-report.css"));
+        copyResource(BASE_PATH + "/css/module-report-index.css", new File(baseDir + "/css/module-report-index.css"));
     }
 
-    private void copyResourceFromWebjar(String resourcePath, File destFile) {
+    private void copyResource(String resourcePath, File destFile) {
         ClassLoader classLoader = getClass().getClassLoader();
         InputStream stream = classLoader.getResourceAsStream(resourcePath);
         try {
             copyInputStreamToFile(stream, destFile);
         } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void copyResource(String resourcePath, File destDir) {
-        ClassLoader classLoader = getClass().getClassLoader();
-        try {
-            copyFileToDirectory(new File(classLoader.getResource(resourcePath).toURI()), destDir);
-        } catch (URISyntaxException | IOException e) {
             throw new RuntimeException(e);
         }
     }
