0640f2352847d3b55ba8f6dc222445a99eea8fa0
Adam Laczynski <adam.laczynski@gmail.com>
2015-02-15 22:23:01 +0100

    move copyResourceToFile to FileUtils

--- coverage/src/main/java/com/github/lizardev/xquery/saxon/coverage/report/StaticResourceTransferor.java
+++ coverage/src/main/java/com/github/lizardev/xquery/saxon/coverage/report/StaticResourceTransferor.java
@@ -1,33 +1,20 @@
 package com.github.lizardev.xquery.saxon.coverage.report;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 
-import static org.apache.commons.io.FileUtils.copyInputStreamToFile;
+import static com.github.lizardev.xquery.saxon.coverage.util.FileUtils.copyResourceToFile;
 
 public class StaticResourceTransferor {
 
-    private static final String BASE_PATH = "com/github/lizardev/xquery/saxon/coverage/report";
-    private static final String WEB_JARS_PATH = "META-INF/resources/webjars";
+    private static final String BASE_REPORT_PATH = "com/github/lizardev/xquery/saxon/coverage/report";
+    private static final String BASE_WEB_JARS_PATH = "META-INF/resources/webjars";
 
-    public void copyResources(File baseDir) {
-        copyResource(WEB_JARS_PATH + "/jquery/2.1.3/jquery.min.js", new File(baseDir + "/js/jquery.min.js"));
-        copyResource(WEB_JARS_PATH + "/tablesorter/2.17.8/js/jquery.tablesorter.min.js", new File(baseDir + "/js/jquery.tablesorter.min.js"));
-        copyResource(WEB_JARS_PATH + "/tablesorter/2.17.8/css/theme.blue.css", new File(baseDir + "/css/theme.blue.css"));
-        copyResource(WEB_JARS_PATH + "/jQuery.FilterTable/1.5/jquery.filtertable.min.js", new File(baseDir + "/js/jquery.filtertable.min.js"));
-        copyResource(BASE_PATH + "/css/module-report.css", new File(baseDir + "/css/module-report.css"));
-        copyResource(BASE_PATH + "/css/module-report-index.css", new File(baseDir + "/css/module-report-index.css"));
+    public void copyResources(File destinationDir) {
+        copyResourceToFile(BASE_WEB_JARS_PATH + "/jquery/2.1.3/jquery.min.js", new File(destinationDir + "/js/jquery.min.js"));
+        copyResourceToFile(BASE_WEB_JARS_PATH + "/tablesorter/2.17.8/js/jquery.tablesorter.min.js", new File(destinationDir + "/js/jquery.tablesorter.min.js"));
+        copyResourceToFile(BASE_WEB_JARS_PATH + "/tablesorter/2.17.8/css/theme.blue.css", new File(destinationDir + "/css/theme.blue.css"));
+        copyResourceToFile(BASE_WEB_JARS_PATH + "/jQuery.FilterTable/1.5/jquery.filtertable.min.js", new File(destinationDir + "/js/jquery.filtertable.min.js"));
+        copyResourceToFile(BASE_REPORT_PATH + "/css/module-report.css", new File(destinationDir + "/css/module-report.css"));
+        copyResourceToFile(BASE_REPORT_PATH + "/css/module-report-index.css", new File(destinationDir + "/css/module-report-index.css"));
     }
-
-    private void copyResource(String resourcePath, File destFile) {
-        ClassLoader classLoader = getClass().getClassLoader();
-        InputStream stream = classLoader.getResourceAsStream(resourcePath);
-        try {
-            copyInputStreamToFile(stream, destFile);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
 }
--- coverage/src/main/java/com/github/lizardev/xquery/saxon/coverage/util/FileUtils.java
+++ coverage/src/main/java/com/github/lizardev/xquery/saxon/coverage/util/FileUtils.java
@@ -2,8 +2,11 @@ package com.github.lizardev.xquery.saxon.coverage.util;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.util.List;
+
+import static org.apache.commons.io.FileUtils.copyInputStreamToFile;
 
 public class FileUtils {
 
@@ -26,6 +29,15 @@ public class FileUtils {
     public static List<String> readLines(URI uri) {
         try {
             return org.apache.commons.io.FileUtils.readLines(new File(uri));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void copyResourceToFile(String sourceResource, File destinationFile) {
+        InputStream sourceStream = FileUtils.class.getClassLoader().getResourceAsStream(sourceResource);
+        try {
+            copyInputStreamToFile(sourceStream, destinationFile);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
