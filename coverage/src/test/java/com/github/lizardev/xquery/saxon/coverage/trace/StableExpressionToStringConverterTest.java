c1d74a8a25c08acb161aa25ca695f31b2e2178f6
Adam Laczynski <adam.laczynski@gmail.com>
2015-04-12 23:36:20 +0200

    #21 support cases when class name is surrounded by expression

--- coverage/src/test/java/com/github/lizardev/xquery/saxon/coverage/trace/StableExpressionToStringConverterTest.java
+++ coverage/src/test/java/com/github/lizardev/xquery/saxon/coverage/trace/StableExpressionToStringConverterTest.java
@@ -12,25 +12,46 @@ import static org.mockito.BDDMockito.given;
 @RunWith(MockitoJUnitRunner.class)
 public class StableExpressionToStringConverterTest {
 
+    private static final String SAXONICA_CLASS_NAME = "com.saxonica.functions.hof.CallableFunctionItem";
+    private static final String SAXONICA_CLASS_NAME_WITH_HASH_CODE_SUFFIX = SAXONICA_CLASS_NAME + "@6bee793f";
+    private static final String EXPRESSION_WITHOUT_CLASS_NAME = "substring-after($nextStep,'@')";
     @Mock private Expression expression;
     private StableExpressionToStringConverter stableExpressionToStringConverter = new StableExpressionToStringConverter();
 
     @Test
     public void shouldRemoveHashCodeSuffixWhenStringRepresentSaxonicaClassWithHashCodeSuffix() {
-        given(expression.toString()).willReturn("com.saxonica.functions.hof.CallableFunctionItem@6bee793f");
+        given(expression.toString()).willReturn(SAXONICA_CLASS_NAME_WITH_HASH_CODE_SUFFIX);
 
         String expressionAsString = stableExpressionToStringConverter.toString(expression);
 
-        assertThat(expressionAsString).isEqualTo("com.saxonica.functions.hof.CallableFunctionItem");
+        assertThat(expressionAsString).isEqualTo(SAXONICA_CLASS_NAME);
+    }
+
+    @Test
+    public void shouldRemoveHashCodeSuffixWhenStringRepresentationContainsSaxonicaClassWithHashCodeSuffix() {
+        given(expression.toString()).willReturn("expression(" + SAXONICA_CLASS_NAME_WITH_HASH_CODE_SUFFIX + ")");
+
+        String expressionAsString = stableExpressionToStringConverter.toString(expression);
+
+        assertThat(expressionAsString).isEqualTo("expression(" + SAXONICA_CLASS_NAME + ")");
     }
 
     @Test
     public void shouldNotCovertWhenStringDoesNotRepresentClassName() {
-        String givenExpressionAsString = "substring-after($nextStep,'@')";
-        given(expression.toString()).willReturn(givenExpressionAsString);
+        given(expression.toString()).willReturn(EXPRESSION_WITHOUT_CLASS_NAME);
 
         String expressionAsString = stableExpressionToStringConverter.toString(expression);
 
-        assertThat(expressionAsString).isEqualTo(givenExpressionAsString);
+        assertThat(expressionAsString).isEqualTo(EXPRESSION_WITHOUT_CLASS_NAME);
+    }
+
+    @Test
+    public void shouldReturnStoredValueWhenTheSameExpressionIsPassedSecondTimeInARow() {
+        given(expression.toString())
+                .willReturn(SAXONICA_CLASS_NAME_WITH_HASH_CODE_SUFFIX)
+                .willReturn(EXPRESSION_WITHOUT_CLASS_NAME);
+
+        assertThat(stableExpressionToStringConverter.toString(expression)).isEqualTo(SAXONICA_CLASS_NAME);
+        assertThat(stableExpressionToStringConverter.toString(expression)).isEqualTo(SAXONICA_CLASS_NAME);
     }
 }
