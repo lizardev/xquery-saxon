f03d92903da0f55d05739e60a1c4dd922cc09068
Michal Jedynak <m.jedynak@gmail.com>
2015-04-23 22:53:39 +0200

    report directory specified by system property

--- coverage/src/main/java/com/github/lizardev/xquery/saxon/coverage/SystemProperties.java
+++ coverage/src/main/java/com/github/lizardev/xquery/saxon/coverage/SystemProperties.java
@@ -1,9 +1,15 @@
 package com.github.lizardev.xquery.saxon.coverage;
 
+import java.util.Optional;
+import java.util.Properties;
+
+import static java.util.Optional.of;
+
 public class SystemProperties {
 
     public static final String COVERAGE_ENABLED = "xquery.saxon.coverage";
     public static final String COVERAGE_REPORT_PRINTING_ON_SHUTDOWN = "xquery.saxon.coverage.report.printing.on.shutdown";
+    public static final String COVERAGE_REPORT_DIRECTORY = "xquery.saxon.coverage.report.directory";
 
     public boolean isCoverageEnabled() {
         return System.getProperties().containsKey(COVERAGE_ENABLED);
@@ -11,5 +17,10 @@ public class SystemProperties {
 
     public boolean isCoverageReportPrintingOnShutdownEnabled() {
         return System.getProperties().containsKey(COVERAGE_REPORT_PRINTING_ON_SHUTDOWN);
+    }
+
+    public Optional<String> getCoverageReportDirectory() {
+        Properties properties = System.getProperties();
+        return properties.containsKey(COVERAGE_REPORT_DIRECTORY) ? of(properties.getProperty(COVERAGE_REPORT_DIRECTORY)) : Optional.<String>empty();
     }
 }
