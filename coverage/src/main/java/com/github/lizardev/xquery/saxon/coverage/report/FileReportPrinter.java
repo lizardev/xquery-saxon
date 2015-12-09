b35bcc1e284b3bf48f28cbf68a50f9b3fe328c63
Daniel Luebke <github@daniel-luebke.de>
2015-12-09 20:58:47 +0100

    #41 Make target directory and file names for report files configurable

--- coverage/src/main/java/com/github/lizardev/xquery/saxon/coverage/report/FileReportPrinter.java
+++ coverage/src/main/java/com/github/lizardev/xquery/saxon/coverage/report/FileReportPrinter.java
@@ -19,7 +19,7 @@ public class FileReportPrinter implements ReportPrinter {
     private HtmlModuleReportGenerator moduleReportGenerator = new HtmlModuleReportGenerator();
     private HtmlModuleReportIndexGenerator moduleReportIndexGenerator = new HtmlModuleReportIndexGenerator();
     private StaticResourceTransferor resourceTransferor = new StaticResourceTransferor();
-
+	
     @Override public void print(Report report) {
         deleteDir(baseDir);
         List<ModuleReportReference> moduleReportReferences = newArrayList();
@@ -33,7 +33,39 @@ public class FileReportPrinter implements ReportPrinter {
         resourceTransferor.copyResources(baseDir);
     }
 
+	/**
+	  * Returns the directory into which the report will be written.
+	  */
+	public File getReportDir() {
+		return baseDir;
+	}
+	
+	/**
+	  * The coverage report consists of many files that are stored in a directory.
+	  * This method can be called to override the default report directory.
+	  *
+	  * <b>Be aware that the directory will be deleted prior to report generation!</b>
+	  *
+	  * @see DEFAULT_REPORT_DIR
+	  */
+	public void setReportDir(File reportDir) {
+		baseDir = reportDir;
+	}
+	
     private File getHtmlModuleReportFile(ModuleReport moduleReport) {
-        return new File(baseDir, uriToFilename(moduleReport.getModuleUri().getUri()) + ".html");
+        return new File(baseDir, generateFilename(moduleReport));
     }
+	
+	/**
+	  * This is a template method for generating a report file name for a given 
+	  * XQuery module.
+	  * The default implementation calls uriToFilename and appends an .html suffix
+	  *
+	  * @param moduleReport the description of the XQuery module
+	  * @return non-null, valid file name ending in .html or .htm
+	  * @see com.github.lizardev.xquery.saxon.coverage.util.UriUtils#uriToFilename
+	  */
+	protected String generateFilename(ModuleReport moduleReport) {
+		return uriToFilename(moduleReport.getModuleUri().getUri()) + ".html";
+	}
 }
